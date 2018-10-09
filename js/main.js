ff49c41c15e63acd32c80cdb3dd524a548cdab50
Bereket <bekiawsome21@gmail.com>
2018-10-09 13:41:37 +0300

    style change and fixing the brand logo thing

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -21,18 +21,22 @@ function main() {
     $(window).bind('scroll', function() {
         var navHeight = $(window).height() - 500;
         if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
+        $('.navbar-default').addClass('on');
+        var x = document.getElementById("navbar-brand123");
+         x.style.color = "white";
         } else {
             $('.navbar-default').removeClass('on');
+            var x = document.getElementById("navbar-brand123");
+             x.style.color = "Red";
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     });
-	
-	
+
+
 	// Hide nav on click
   $(".navbar-nav li a").click(function (event) {
     // check if window is small enough so dropdown is created
@@ -69,11 +73,11 @@ function main() {
         });
 
     });
-	
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
